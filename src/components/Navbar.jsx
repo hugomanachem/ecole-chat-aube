ecbc8cda709cc5c933a4588696fd34c32f6ad335
Hugo Manachem <hugomanachem.pro@gmail.com>
2024-02-27 16:49:19 +0100

    Clean useless files + initial Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import navbar_logo from "../assets/navbar-logo.png";
+import "../navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="primary-bg-color navbar">
+      <div className="navbar-content">
+        <img src={navbar_logo}></img>
+        <div className="navbar-links">
+          <a className="tertiary-color">Accueil</a>
+          <a className="tertiary-color">Faire un don</a>
+          <a className="tertiary-color">Devenir volontaire</a>
+          <a className="tertiary-color">Nos chats</a>
+          <a className="tertiary-color">Contact</a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;

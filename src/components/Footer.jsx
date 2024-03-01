5bd735f849aa9c05e084a554f2cae677e21d9648
Hugo Manachem <hugomanachem.pro@gmail.com>
2024-03-01 14:12:27 +0100

    Footer : generalize

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,12 @@
 import footer_logo from '../assets/footer/footer_logo.png';
+import facebook_logo_white from '../assets/footer/facebook_logo_white.png';
+import home_logo_white from '../assets/footer/home_logo_white.png'; 
+import phone_logo_white from '../assets/footer/phone_logo_white.png'; 
+import email_logo_white from '../assets/footer/email_logo_white.png';  
+import facebook_logo_yellow from '../assets/footer/facebook_logo_yellow.png';
+import home_logo_yellow from '../assets/footer/home_logo_yellow.png'; 
+import phone_logo_yellow from '../assets/footer/phone_logo_yellow.png'; 
+import email_logo_yellow from '../assets/footer/email_logo_yellow.png'; 
 
 function Footer (props) {
 
@@ -6,12 +14,21 @@ function Footer (props) {
         <footer className={props.bg_color}>
             <div className="footer-content">
                 <img src={footer_logo}/>
+                {props.bg_color && props.bg_color == "primary-bg-color" ? 
                 <div className="footer-links">
-                    <a className='tertiary-color'><img className='footer-links-logo' src={`src/assets/footer/facebook_logo_${props.icons_color}.png`}/>Notre Facebook</a>
-                    <a className='tertiary-color'><img className='footer-links-logo' src={`src/assets/footer/home_logo_${props.icons_color}.png`}/>B.P. 80 176 - 10605 La Chapelle Saint Luc Cedex</a>
-                    <a className='tertiary-color'><img className='footer-links-logo' src={`src/assets/footer/phone_logo_${props.icons_color}.png`}/>09.80.38.44.40 - 06.28.84.25.65</a>
-                    <a className='tertiary-color'><img className='footer-links-logo' src={`src/assets/footer/email_logo_${props.icons_color}.png`}/>contact@ecoleduchatdelaube.com</a>
+                    <a className='tertiary-color'><img className='footer-links-logo' src={facebook_logo_yellow}/>Notre Facebook</a>
+                    <a className='tertiary-color'><img className='footer-links-logo' src={home_logo_yellow}/>B.P. 80 176 - 10605 La Chapelle Saint Luc Cedex</a>
+                    <a className='tertiary-color'><img className='footer-links-logo' src={phone_logo_yellow}/>09.80.38.44.40 - 06.28.84.25.65</a>
+                    <a className='tertiary-color'><img className='footer-links-logo' src={email_logo_yellow}/>contact@ecoleduchatdelaube.com</a>  
                 </div>
+                :
+                <div className="footer-links">
+                    <a className='tertiary-color'><img className='footer-links-logo' src={facebook_logo_white}/>Notre Facebook</a>
+                    <a className='tertiary-color'><img className='footer-links-logo' src={home_logo_white}/>B.P. 80 176 - 10605 La Chapelle Saint Luc Cedex</a>
+                    <a className='tertiary-color'><img className='footer-links-logo' src={phone_logo_white}/>09.80.38.44.40 - 06.28.84.25.65</a>
+                    <a className='tertiary-color'><img className='footer-links-logo' src={email_logo_white}/>contact@ecoleduchatdelaube.com</a>  
+                </div>
+                }
             </div>
         </footer>
         

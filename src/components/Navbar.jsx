603e494efc7637371ba5e81d02c4aa61f1e9356f
Hugo Manachem <hugomanachem.pro@gmail.com>
2024-03-21 16:03:53 +0100

    Responsive Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,19 @@
 import navbar_logo from "../assets/navbar_logo.png";
 import { Link } from "react-router-dom";
+import hamburger_menu_img from "../assets/hamburger-menu.png";
 
 function Navbar(props) {
+  function ToggleResponsiveNavbar() {
+    let navbarElm = document.getElementById("navbar");
+    if (navbarElm.className === "navbar") {
+      navbarElm.classList.toggle("responsive");
+    } else {
+      navbarElm.classList.toggle("responsive");
+    }
+  }
+
   return (
-    <nav className={`${props.bg_color}-bg-color navbar`}>
+    <nav id="navbar" className={`${props.bg_color}-bg-color navbar`}>
       <div className="navbar-content">
         <img src={navbar_logo}></img>
         <div className="navbar-links">
@@ -13,6 +23,13 @@ function Navbar(props) {
           <Link to={"/chats"}>Nos chats</Link>
           <Link to={"/contact"}>Contact</Link>
         </div>
+        <Link onClick={ToggleResponsiveNavbar}>
+          <img
+            className="hamburger-icon"
+            src={hamburger_menu_img}
+            alt="hamburger-menu"
+          />
+        </Link>
       </div>
     </nav>
   );
